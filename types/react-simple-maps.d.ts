17868b87a8830081f391e39bdb141b50068e9759
jetpackmatt <matt@shipwithjetpack.com>
2025-11-28 09:02:18 -0500

    Add TypeScript declarations for react-simple-maps

---
+++ types/react-simple-maps.d.ts
@@ -0,0 +1,74 @@
+declare module 'react-simple-maps' {
+  import { ComponentType, ReactNode } from 'react'
+
+  export interface ComposableMapProps {
+    projection?: string
+    projectionConfig?: {
+      scale?: number
+      center?: [number, number]
+      rotate?: [number, number, number]
+    }
+    width?: number
+    height?: number
+    children?: ReactNode
+    style?: React.CSSProperties
+    className?: string
+  }
+
+  export interface GeographiesProps {
+    geography: string | object
+    children: (props: { geographies: Geography[] }) => ReactNode
+  }
+
+  export interface Geography {
+    rsmKey: string
+    properties: {
+      name: string
+      [key: string]: unknown
+    }
+    geometry: unknown
+  }
+
+  export interface GeographyProps {
+    geography: Geography
+    style?: {
+      default?: React.CSSProperties
+      hover?: React.CSSProperties
+      pressed?: React.CSSProperties
+    }
+    onMouseEnter?: (event: React.MouseEvent) => void
+    onMouseLeave?: (event: React.MouseEvent) => void
+    onClick?: (event: React.MouseEvent) => void
+    fill?: string
+    stroke?: string
+    strokeWidth?: number
+    className?: string
+  }
+
+  export interface MarkerProps {
+    coordinates: [number, number]
+    children?: ReactNode
+    style?: {
+      default?: React.CSSProperties
+      hover?: React.CSSProperties
+      pressed?: React.CSSProperties
+    }
+  }
+
+  export interface ZoomableGroupProps {
+    center?: [number, number]
+    zoom?: number
+    minZoom?: number
+    maxZoom?: number
+    onMoveStart?: (position: { coordinates: [number, number]; zoom: number }) => void
+    onMove?: (position: { coordinates: [number, number]; zoom: number }) => void
+    onMoveEnd?: (position: { coordinates: [number, number]; zoom: number }) => void
+    children?: ReactNode
+  }
+
+  export const ComposableMap: ComponentType<ComposableMapProps>
+  export const Geographies: ComponentType<GeographiesProps>
+  export const Geography: ComponentType<GeographyProps>
+  export const Marker: ComponentType<MarkerProps>
+  export const ZoomableGroup: ComponentType<ZoomableGroupProps>
+}
